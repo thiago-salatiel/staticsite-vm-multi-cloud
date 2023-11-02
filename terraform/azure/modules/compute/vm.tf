02c48039e3ef8154609bca38eb83e67b9f16e605
Thiago Salatiel <30-71745@engelogtec.com.br>
2023-11-01 21:31:53 -0300

    Add RDP Port com Checkov

--- terraform/azure/modules/compute/vm.tf
+++ terraform/azure/modules/compute/vm.tf
@@ -43,6 +43,17 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1040
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "nic" {
